0b00980a1eebf4d387fcec8859d8af6afdaa5cb9
Mateusz Wiącek <68820849+MarioPeperoni@users.noreply.github.com>
2023-08-26 16:04:10 +0200

    Authentication modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Sidebar from '@/components/Sidebar';
 
 import SupabaseProvider from '@/providers/SupabaseProvider';
 import UserProvider from '@/providers/UserProvider';
+import ModalProvider from '@/providers/ModalProvider';
 
 const font = Figtree({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={font.className}>
         <SupabaseProvider>
           <UserProvider>
+            <ModalProvider />
             <Sidebar>{children}</Sidebar>
           </UserProvider>
         </SupabaseProvider>
---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import AuthModal from '@/components/AuthModal';
+import { useEffect, useState } from 'react';
+
+const ModalProvider = () => {
+  const [isMouted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMouted) return null;
+
+  return (
+    <>
+      <AuthModal />
+    </>
+  );
+};
+
+export default ModalProvider;
