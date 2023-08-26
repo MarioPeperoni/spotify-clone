f9169b74b57694ed4831b164a3afe0400cad6597
Mateusz Wiącek <68820849+MarioPeperoni@users.noreply.github.com>
2023-08-26 14:52:18 +0200

    Providers for auth and supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import { Figtree } from 'next/font/google';
 
 import Sidebar from '@/components/Sidebar';
 
+import SupabaseProvider from '@/providers/SupabaseProvider';
+import UserProvider from '@/providers/UserProvider';
+
 const font = Figtree({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -15,7 +18,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>{children}</Sidebar>
+        <SupabaseProvider>
+          <UserProvider>
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
       </body>
     </html>
   );
